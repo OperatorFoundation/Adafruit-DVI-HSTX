4e32407572a74955cb79b703a9529cee1a0d73e8
Dr. Brandon Wiley <brandon@operatorfoundation.org>
2025-07-02 23:59:04 -0500

    Fixed timing struct name

--- src/drivers/dvhstx/dvhstx.cpp
+++ src/drivers/dvhstx/dvhstx.cpp
@@ -515,7 +515,7 @@ bool DVHSTX::init(uint16_t width, uint16_t height, Mode mode_, bool double_buffe
         frame_height = TERMINAL_ROWS_720;
         h_repeat_shift = 0;
         v_repeat_shift = 0;
-        timing_mode = &dvi_timing_waveshare_720x1280_timing;
+        timing_mode = &dvi_timing_waveshare_720x1280;
     }
     else if (width == 320 && height == 180) {
         h_repeat_shift = 2;
